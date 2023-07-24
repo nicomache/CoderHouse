6da26fa9797ae2f7c29e81a4d2132045e9db014f
Nicolas Morales <138627619+nicomache@users.noreply.github.com>
2023-07-24 19:44:34 -0300

    zz

--- objeto.js
+++ objeto.js
@@ -1,18 +1,21 @@
-let nombre = prompt("ingrese el nombre")
-let edad = prompt("ingrese la edad");
-let direccion =prompt("ingrese la direccion");
 
 
-function Persona (nombre, edad, direccion){
-    this.nombre = nombre;
-    this.edad = edad;
-    this.direccion = direccion;
+let nombre = prompt("Ingrese el nombre");
+let edad = prompt("Ingrese la edad");
+let direccion = prompt("Ingrese la dirección");
+
+function Persona(nombre, edad, direccion) {
+  this.nombre = nombre;
+  this.edad = edad;
+  this.direccion = direccion;
 }
 
+const persona1 = new Persona(nombre, edad, direccion);
 
+// Crear un mensaje con los atributos de persona1
+let mensaje = "Nombre: " + persona1.nombre + "\n" +
+              "Edad: " + persona1.edad + "\n" +
+              "Dirección: " + persona1.direccion;
 
-const persona1 = new Persona (nombre, edad, direccion)
-
-console.log("Nombre:", persona1.nombre);
-console.log("Edad:", persona1.edad);
-console.log("Dirección:", persona1.direccion);
+// Mostrar el mensaje con alert
+alert(mensaje);
